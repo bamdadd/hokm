79619e3a445ff315a0581640fdb0139f362cc3d9
Bamdad Manchester <bdashtban@yahoo.com>
2012-02-24 18:12:01 +0000

    Console UI , show cards and Hakem

--- lib/game_view.rb
+++ lib/game_view.rb
@@ -1,26 +1,63 @@
 require 'colorize'
-require "suit"
-
 class GameView
   def initialize
 
   end
 
-  def show_table(player_number)
+  def show_table(player_number,hakem_number)
     case player_number
       when 1
-        puts "            |p3|                 "
-        puts "     |P2|          |p4|          "
-        puts "            |you|                ".red
+        case hakem_number
+          when 1
+            puts "            |p3|                 "
+            puts "     |P2|          |p4|          "
+            puts "            |you|".red+get_hakem_symbol.yellow
+          when 2
+            puts "            |p3|                 "
+            puts "     |P2|"+get_hakem_symbol.yellow+"         |p4|          "
+            puts "            |you|".red
+          when 3
+            puts "            |p3|"+get_hakem_symbol.yellow
+            puts "     |P2|          |p4|          "
+            puts "            |you|".red
+          when 4
+            puts "            |p3|                 "
+            puts "     |P2|          |p4|"+get_hakem_symbol.yellow
+            puts "            |you|".red
+        end
+
 
       when 2
-        puts "            |p3|                 "
-        puts "     |P2|".red+"          |p4|   "
-        puts "            |you|                "
+        case hakem_number
+          when 1
+            puts "            |p3|                 "
+            puts "     |P2|".red+"          |p4|   "
+            puts "            |you|"+get_hakem_symbol.yellow
+          when 2
+            puts "            |p3|                 "
+            puts "     |P2|".red+get_hakem_symbol.yellow+"         |p4|          "
+            puts "            |you|".red
+          when 3
+            puts "            |p3|"+get_hakem_symbol.yellow
+            puts "     |P2|".red+"          |p4|   "
+            puts "            |you|"
+          when 4
+            puts "            |p3|"
+            puts "     |P2|".red+"          |p4|"+get_hakem_symbol.yellow
+            puts "            |you|"
+        end
+
       when 3
-        puts "            |p3|                 ".red
-    puts "     |P2|          |p4|          "
-    puts "            |you|                "
+        case hakem_number
+          when 1
+            puts "            |p3|                 ".red
+            puts "     |p2|"+get_hakem_symbol.yellow+"         |p4|          "
+            puts "            |you|"+get_hakem_symbol.yellow
+          when 2
+          when 3
+          when 4
+        end
+
       when 4
         puts "            |p3|                 "
     puts "     |P2|         "+"|p4|          ".red
@@ -29,27 +66,31 @@ class GameView
   end
 
   def show_hand(hand)
-    puts "-----------------------"
-    hand
-    hand.each{|card|
+    puts "---------------------------------------------------------------------"
+    result="|"
+    for index in 0..hand.size-1
+     card=hand[index]
       if (card.number<=10)
-        puts "#{card.number}#{get_symbol card.suit}"
+        result+= "#{index}=".white+"#{card.number}#{get_symbol card.suit}|"
       elsif card.number==11
-        puts "J#{get_symbol card.suit}"
+        result+= "#{index}=".white+"J#{get_symbol card.suit}|"
       elsif card.number==12
-        puts "Q#{get_symbol card.suit}"
+        result+= "#{index}=".white+"Q#{get_symbol card.suit}|"
       elsif (card.number==13)
-        puts "K#{get_symbol card.suit}"
+        result+= "#{index}=".white+ "K#{get_symbol card.suit}|"
       end
-
-    }
-    puts "-----------------------"
+    end
+    puts result
+    puts "----------------------------------------------------------------------"
   end
   def get_symbol(suit)
     return "♣" if suit== Suit::CLUB
     return "♥".red if suit== Suit::HEART
     return "♦".red if suit== Suit::DIAMOND
     return "♠" if suit== Suit::SPADE
+  end
 
+  def get_hakem_symbol
+    return "♚"
   end
 end
--- spec/game_view_spec.rb
+++ spec/game_view_spec.rb
@@ -8,10 +8,10 @@ describe GameView do
    @gv=GameView.new
   end
   it "should show table" do
-    @gv.show_table 1
-    @gv.show_table 2
-    @gv.show_table 3
-    @gv.show_table 4
+    @gv.show_table 1,4
+    @gv.show_table 2,4
+    @gv.show_table 3,3
+    @gv.show_table 4,4
   end
 
   it "should show a hand" do
@@ -19,6 +19,15 @@ describe GameView do
     deck.shuffle!
     hand=Hand.new(deck.getQuarter(1))
     @gv.show_hand(hand.hand)
+  end
+
+  it "show 5 cards to hakem to hokm" do
 
   end
+
+  it "should show current hokm" do
+
+  end
+
+
 end
